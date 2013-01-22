ad14d586404baa0820df3d77560ccc0b510d1880
ande3577 <ande3577@gmail.com>
2013-01-21 18:31:26 -0600

    Tweak to routes, added initial update script example

--- app/controllers/git_update_controller.rb
+++ app/controllers/git_update_controller.rb
@@ -1,7 +1,15 @@
 class GitUpdateController < ApplicationController
   unloadable
 
-
+  before_filter :find_project
+  
   def index
+    project = @project
+  end
+  
+  def find_project
+    render_404 if params[:proj_name].nil?
+        
+    @project = Project.where(:name => params[:proj_name]).first
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
-get 'gitupdate', :to => "git_update#index" 
+get ':proj_name/gitupdate', :to => "git_update#index" 
