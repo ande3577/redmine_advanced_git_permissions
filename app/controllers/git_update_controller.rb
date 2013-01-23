8bcd028122a9e9a27cabe8d43c7355d4a17d9c32
ande3577 <ande3577@gmail.com>
2013-01-22 21:25:19 -0600

    refactor to use before filter to validate branch first add empty functions for legal/protected branch check

--- app/controllers/git_update_controller.rb
+++ app/controllers/git_update_controller.rb
@@ -3,10 +3,11 @@ class GitUpdateController < ApplicationController
 
   before_filter :find_project, :find_user
   
+  append_before_filter :validate_branch, :only => [ :create_branch, :delete_branch, :update_branch ]
+  append_before_filter :validate_tag, :only => [ :create_tag, :delete_tag, :update_tag ]
+    
   def create_branch
-    if params[:branch].nil?
-      render_404
-    elsif User.current.allowed_to?(:create_branch, @project)
+    if User.current.allowed_to?(:create_branch, @project)
       render_api_ok
     else
       render_403
@@ -14,14 +15,14 @@ class GitUpdateController < ApplicationController
   end
   
   def delete_branch
-    if params[:branch].nil?
-      render_404
-    elsif User.current.allowed_to?(:delete_branch, @project)
+    if User.current.allowed_to?(:delete_branch, @project)
       render_api_ok
     else
       render_403
     end
-  end  
+  end
+  
+private 
   def find_project
     if params[:proj_name].nil?
       render_404
@@ -44,4 +45,43 @@ class GitUpdateController < ApplicationController
     User.current = User.where(:login => params[:user_name]).first
     true
   end
+  
+  def validate_branch
+    if params[:branch].nil? or !legal_branch(params[:branch])
+       render_404
+       return false
+    elsif protected_branch(params[:branch]) and !User.current().allowed_to?(:update_protected_branch, @project)
+      render_403
+      return false
+    end
+    true
+  end
+  
+  def validate_tag
+    if params[:tag].nil? or !legal_branch(params[:tag])
+       render_404
+       return false
+    elsif protected_tag(params[:tag]) and !User.current().allowed_to?(:update_protected_tag, @project)
+      render_403
+      return false
+    end
+    true
+  end
+
+  def legal_branch(branch)
+    true
+  end
+
+  def protected_branch(branch)
+    false
+  end
+  
+  def legal_tag(tag)
+    true
+  end
+  
+  def protected_tag(tag)
+    false
+  end
+  
 end
