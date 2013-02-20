4b0b311012e6a6103ca761f017c570add5c20047
ande3577 <ande3577@gmail.com>
2013-02-19 19:20:36 -0600

    fixed migration for adding inherit global

--- db/migrate/002_add_inherit_global_to_repository.rb
+++ db/migrate/002_add_inherit_global_to_repository.rb
@@ -1,5 +1,5 @@
 class AddInheritGlobalToRepository < ActiveRecord::Migration
   def change
-    add_column :repository_id, :inherit_global_rules, :boolean, :default => false 
+    add_column :repositories, :inherit_global_rules, :boolean, :default => false 
   end
 end
